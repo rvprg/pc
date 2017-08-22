ca797b84a5d9a6cc51e8ba2263107e28c16ba0b0
Roman Valiusenko <roman.valiusenko@gmail.com>
2017-08-22 21:23:43 +0100

    Improved solution.

--- src/Chapter5/Ones.java
+++ src/Chapter5/Ones.java
@@ -12,19 +12,12 @@ class Ones {
         if (n == 1) {
             return 1;
         }
-        int l = 2;
-        int s = 11;
-        int r = 11;
-
-        if (n != s) {
-            do {
-                s = r * 10 + 1;
-                int d = s / n;
-                r = s - (n * d);
-                l++;
-            } while (r > 0);
-        }
-
+        int l = 0;
+        int r = 0;
+        do {
+            r = (r * 10 + 1) % n;
+            l++;
+        } while (r > 0);
         return l;
     }
 
